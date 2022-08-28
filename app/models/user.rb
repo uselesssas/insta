b209ab80851f1d61fe4e4d835a109312cdc49771
uselesssas <sas.clicking@mail.ru>
2022-08-28 19:27:07 +0300

    Added username field when registering a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, length: { in: 3..25 }, uniqueness: { case_sensitive: false }
 end
